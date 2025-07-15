9894caa4ee1e9d33930c9e4ccb40ea974ec79d1e
Mdzaidsiddique <45.mdzaid@gmail.com>
2025-07-15 21:05:30 +0530

    few code added

---
+++ src/com/datatypes/DataTypes.java
@@ -0,0 +1,9 @@
+package com.datatypes;
+
+public class DataTypes {
+    /*
+        - java is strictly a typed programming language
+        - before representing data, first we have to confirm which type of data we are representing
+        - to represent we have to type of data, we have to use data types
+     */
+}
---
+++ src/com/datatypes/WrapperClass.java
@@ -0,0 +1,25 @@
+package com.datatypes;
+
+public class WrapperClass {
+
+/*  - class representation of primitive data types
+    - for each wrapper class java provided constant variable, MIN_VALUE & MAX_VALUE to identify min & max value
+    - these variables comes from java.lang package
+
+    java.lang.Byte;
+    java.lang.Short;
+    java.lang.Integer;
+    java.lang.Long;
+    java.lang.Float;
+    java.lang.Double;
+    java.lang.Character;
+    java.lang.Boolean; (Boolean.MIN_VALUE & Boolean.MAX_VALUE -> error)
+
+    - there are many other uses of wrapper classes (autoboxing, unboxing, null handling, generics, collections, parsing string, etc)
+*/
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+    }
+
+}
