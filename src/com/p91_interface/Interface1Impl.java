4ef17b5bd96120cd884ddbe8c488850b1afb5b27
Mdzaidsiddique <45.mdzaid@gmail.com>
2025-07-23 20:52:49 +0530

    interface notes and code added

---
+++ src/com/p91_interface/Interface1Impl.java
@@ -0,0 +1,11 @@
+package com.p91_interface;
+
+public class Interface1Impl implements Interface1 {
+
+    @Override
+    public void abstractMethod() {
+        System.out.println("method implemented inside interface impl class");
+    }
+
+}
+
