7c14e87c42ac9d492ac6c3b4960ce2ddd46d9e5b
Mdzaidsiddique <45.mdzaid@gmail.com>
2025-07-24 12:40:42 +0530

    all interface, bindings related code added

---
+++ src/com/p92_binding/Main.java
@@ -0,0 +1,15 @@
+package com.p92_binding;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //early binding
+        Animal an = new Animal();
+        an.sound(); // early binding
+
+
+        // late binding
+        Animal a = new Dog(); // reference type is Animal, object is Dog
+        a.sound(); // late binding: resolved at runtime, Dog's sound() is called
+    }
+}
