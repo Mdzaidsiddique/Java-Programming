049c053520550e0f245312d1128191d2a26b0a61
Mdzaidsiddique <45.mdzaid@gmail.com>
2025-07-30 15:23:41 +0530

    added

--- src/com/p93_exceptions/Main.java
+++ src/com/p93_exceptions/Main.java
@@ -3,7 +3,8 @@ package com.p93_exceptions;
 public class Main {
 
     public static void main(String[] args) {
-        int i = 100/0; // statement may raise exception
-        System.out.println(i);
+//        int i = 100/0; // statement may raise exception
+//        System.out.println(i);
+
     }
 }
