383b9b5aa9a99b24713785a87b09472d86d5dd0e
Mdzaidsiddique <45.mdzaid@gmail.com>
2025-08-21 23:57:02 +0530

    generics and cv, contra var

---
+++ src/com/p990_generics/CovarianceExample.java
@@ -0,0 +1,14 @@
+package com.p990_generics;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class CovarianceExample {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
