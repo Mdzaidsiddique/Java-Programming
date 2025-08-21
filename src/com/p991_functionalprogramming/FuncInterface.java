2f5293be7acda4e407979fba18eaba0b01fe8ad9
Mdzaidsiddique <45.mdzaid@gmail.com>
2025-08-22 01:20:20 +0530

    functional interface

---
+++ src/com/p991_functionalprogramming/FuncInterface.java
@@ -0,0 +1,20 @@
+package com.p991_functionalprogramming;
+
+public interface FuncInterface {
+
+    // only one abstract method
+    void sayHello(String name); // its like public abstract void sayHello(String name);
+
+    //Note: abstract kw is not necessary because all methods in interfaces were implicitly public abstract
+
+    default void defaultMethod(){
+        System.out.println("inside default method of interface one");
+    }
+
+    static void staticMethod(){
+        System.out.println("inside static method of interface one");
+    }
+
+    // method of object class
+    public boolean equals(Object o);
+}
